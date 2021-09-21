93a14aeaa87b475aadb4cf7a2ffadd7b1661a5e2
Hasan Lam <hcb.lam@gmail.com>
2021-09-21 18:13:41 +0000

    Cursor spinner to practice using setTimeout

---
+++ spinner.js
@@ -0,0 +1,23 @@
+
+process.stdout.write('hello from spinner1.js...\rhey\n');
+
+
+setTimeout(() => {
+  process.stdout.write('\r|     ')
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/     ')
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-     ')
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\     ')
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|     ')
+}, 900);
